d6609371b62918767cccf8b17d6c4ffe37a1544a
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2019-10-18 09:38:20 -0300

    Agrege user & grant, diagrama de clases

--- Scripts/Base-de-Datos/MySQL/04 ug.sql
+++ Scripts/Base-de-Datos/MySQL/04 ug.sql
@@ -1,7 +1,23 @@
-create user Administrador@'%' identified by 'contraAdmin';
+create user 'Administrador'@'%' identified by 'contraAdmin';
 
-create user Componente@'%' identified by 'contraComponente';
+create user 'Componente'@'10.120.0.%' identified by 'contraComponente';
 
-create user Cliente@'%' identified by 'contraCliente';
+create user 'Cliente'@'%' identified by 'contraCliente';
 
-grant
+grant select,insert,update (precio) on Fichin to 'Administrador'@'%';
+
+grant select,insert on Cliente to 'Administrador'@'%';
+
+grant select,insert on Cliente to 'Administrador'@'%';
+
+grant insert on Jugada to 'Componente'@'10.120.0.%';
+
+grant update (saldo) on Cliente to 'Componente'@'10.120.0.%';
+
+grant select on Cliente to 'Cliente'@'%';
+
+grant select on Tarjeta to 'Cliente'@'%';
+
+grant select on Jugada to 'Cliente'@'%';
+
+grant insert 
